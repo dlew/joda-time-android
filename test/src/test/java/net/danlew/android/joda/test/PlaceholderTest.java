f1fff7b8b0a499878b5269adaeabe16cddd3f666
Dan Lew <danlew42@gmail.com>
2014-01-16 19:01:50 -0600

    Added basic robolectric test template

---
+++ test/src/test/java/net/danlew/android/joda/test/PlaceholderTest.java
@@ -0,0 +1,17 @@
+package net.danlew.android.joda.test;
+
+import android.app.Activity;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(RobolectricTestRunner.class)
+public class PlaceholderTest {
+
+  @org.junit.Test
+  public void testSomething() throws Exception {
+    assertTrue("" != null);
+  }
+}
