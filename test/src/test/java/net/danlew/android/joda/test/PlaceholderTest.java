659c4d59a5688f094dfd4cf9ccc996cdf8827a03
Dan Lew <danlew42@gmail.com>
2014-01-23 19:42:07 -0600

    Added gradle-based builds
    Removed Robolectric/Junit 4 testing stuff.  Doesn't work well with gradle right
    now.  While I might be annoyed at that, Maven unit testing of AARs is in an
    even worse state right now.

--- test/src/test/java/net/danlew/android/joda/test/PlaceholderTest.java
+++ test/src/test/java/net/danlew/android/joda/test/PlaceholderTest.java
@@ -1,17 +1,10 @@
 package net.danlew.android.joda.test;
 
-import android.app.Activity;
-import org.junit.runner.RunWith;
-import org.robolectric.Robolectric;
-import org.robolectric.RobolectricTestRunner;
+import android.test.InstrumentationTestCase;
 
-import static org.junit.Assert.assertTrue;
+public class PlaceholderTest extends InstrumentationTestCase {
 
-@RunWith(RobolectricTestRunner.class)
-public class PlaceholderTest {
-
-  @org.junit.Test
   public void testSomething() throws Exception {
-    assertTrue("" != null);
+  	assertEquals(true, true);
   }
 }
