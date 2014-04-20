c0b586fc782dc4db1d180bfd8ac316fbc43879fe
Dan Lew <danlew42@gmail.com>
2014-04-20 09:44:25 -0500

    Initialize joda-time-android in sample

---
+++ sample/src/main/java/net/danlew/android/joda/sample/JodaApplication.java
@@ -0,0 +1,16 @@
+package net.danlew.android.joda.sample;
+
+import android.app.Application;
+import net.danlew.android.joda.ResourceZoneInfoProvider;
+
+public class JodaApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // It's important to initialize the ResourceZoneInfoProvider; otherwise
+        // joda-time-android will not work.
+        ResourceZoneInfoProvider.init(this);
+    }
+}
