48e9f19cadac0e1c3bf8fd60577281dce013b03a
Dan Lew <danlew42@gmail.com>
2014-04-20 09:54:58 -0500

    Added easy way to add samples

--- sample/src/main/java/net/danlew/android/joda/sample/JodaSampleActivity.java
+++ sample/src/main/java/net/danlew/android/joda/sample/JodaSampleActivity.java
@@ -2,14 +2,29 @@ package net.danlew.android.joda.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class JodaSampleActivity extends Activity {
+
+    private ViewGroup mContent;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_joda_sample);
+
+        mContent = (ViewGroup) findViewById(R.id.content);
+    }
+
+    private void addSample(CharSequence title, CharSequence text) {
+        View view = LayoutInflater.from(this).inflate(R.layout.include_sample, mContent, false);
+        ((TextView) view.findViewById(R.id.title)).setText(title);
+        ((TextView) view.findViewById(R.id.text)).setText(text);
+        mContent.addView(view);
     }
 
 }
