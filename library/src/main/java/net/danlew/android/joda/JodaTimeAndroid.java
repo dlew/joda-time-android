eaa318b1977805b87fe5d29d0e25bef6a2966463
Dan Lew <danlew42@gmail.com>
2020-12-27 07:07:04 -0600

    Updated to tzdata 2020e

--- library/src/main/java/net/danlew/android/joda/JodaTimeAndroid.java
+++ library/src/main/java/net/danlew/android/joda/JodaTimeAndroid.java
@@ -7,7 +7,7 @@ import android.content.Context;
  */
 public final class JodaTimeAndroid {
 
-    public static String TZ_DATA_VERSION = "2020d";
+    public static String TZ_DATA_VERSION = "2020e";
 
     private JodaTimeAndroid() {
         // no instances
