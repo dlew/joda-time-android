cb8c7f3ae0d9b36205a3fd9b61593f5a614baaea
Dan Lew <danlew42@gmail.com>
2023-03-24 07:47:10 -0500

    Updated to 2023bgtz
    If you're wondering where 2023agtz went, it was out for all of one day
    before a new version superceded it.

--- library/src/main/java/net/danlew/android/joda/JodaTimeAndroid.java
+++ library/src/main/java/net/danlew/android/joda/JodaTimeAndroid.java
@@ -7,7 +7,7 @@ import android.content.Context;
  */
 public final class JodaTimeAndroid {
 
-    public static String TZ_DATA_VERSION = "2022ggtz";
+    public static String TZ_DATA_VERSION = "2023bgtz";
 
     private JodaTimeAndroid() {
         // no instances
